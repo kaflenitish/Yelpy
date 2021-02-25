49b9cfb5ea00c3e11239d24cd24ff35aa2e79e30
Nitish Kafle <kaflenit@gmail.com>
2021-02-24 19:46:24 -0500

    Initial Commit

---
+++ Yelpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Nitish Kafle on 2/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
